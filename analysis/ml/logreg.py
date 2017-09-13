f5ac16500e2ddb655a052d83ba4935c88950aa75
Chameleon Cloud User <ahaider3@hawk.iit.edu>
2017-09-13 16:10:52 +0000

    updated logreg model w/ sklearn

--- analysis/ml/logreg.py
+++ analysis/ml/logreg.py
@@ -1,6 +1,15 @@
+from sklearn import linear_model
+import pickle
 
+def logreg(x, y, output_path, test_data=None):
 
-
-def logreg(x, y):
-
-  
+  logreg = linear_model.LogisticRegression()
+  accuracy = None
+  logreg.fit(x, y)
+  with open(output_path, "w") as f:
+    pickle.dump(logreg, f)
+  if test_data:
+     accuracy = np.mean(np.square(test_data[1] - logreg.predict(test_data[0])))
+     print("Accuracy:", accuracy)
+  print("Wrote trained model")
+  return accuracy
