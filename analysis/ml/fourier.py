601b1326ccd98c42d88f9d35143f10e0a3aa5556
adnan <ahaider3@hawk.iit.edu>
2017-09-20 09:56:30 -0400

    Updated fourier analysis

--- analysis/ml/fourier.py
+++ analysis/ml/fourier.py
@@ -1,3 +1,40 @@
-from mongo_funcs import 
+import sys 
+sys.path.append("/home/adnan/py_nlp/analysis/io/")
+from mongo_funcs import *
+import numpy as np
+from datetime import datetime
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
-data = 
+data = list(read("tweet_frequency"))
+out_data = []
+d_times = defaultdict(list)
+for d in data:
+  r = {}
+  r['time'] = d['time']
+  r['rt'] = datetime.strptime(d['time'].split('.')[0], '%Y-%m-%d %X')
+  r['freq'] = d['frequency']
+  r['type'] = d['type']
+  d_times[d['type']].append(r)
+for d in d_times:
+  d_times[d] = sorted(d_times[d], key= lambda t: t['rt'])
+  ys = [y['freq'] for y in d_times[d]]
+  mean = float(sum(ys))/len(ys)
+  ys = [y for y in ys]
+  rfft = np.fft.rfft(ys)
+  rfft[10:] = 0
+
+  irfft = np.fft.irfft(rfft)
+  print(len(irfft), len(d_times[d]))
+#  assert(len(irfft) == len(d_times[d]))
+  for ind, k in enumerate(irfft):
+    d_times[d][ind]['irfft'] = float(k)
+    
+  plt.plot(irfft)
+
+for d in d_times:
+  out_data += d_times[d]
+
+
+write(out_data, "tweet_frequency_smooth")
+
